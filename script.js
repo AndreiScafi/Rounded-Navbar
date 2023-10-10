90c0b9ad41231e5038f536bfdeb826e006bdc243
Andrei Scafi <andreiscafi@hotmail.com>
2023-10-10 06:51:25 -0300

    Adding transition effect to the button.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const btn = document.querySelector('.navbar-btn');
-const navbar = document.querySelector('.navbar');
+const navbar = document.querySelector('.navbar-wrapper');
 
 btn.addEventListener('click', () => {
     navbar.classList.toggle('change');
